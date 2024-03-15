abcde6224e0615a709ab45c688af111df1d53b0b
daoendi <dangdao102.5@gmail.com>
2024-03-15 14:53:49 +0700

    Commit

--- Tuan 4/bai4.cpp
+++ Tuan 4/bai4.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 using namespace std;
+char a[1000][1000] = {};
+int c[8]={-1,0,1,1,1,0,-1,-1};
+int h[8]={-1,-1,-1,0,1,1,1,0};
+int dmin(int x,int y)
+{
+    int res = 0;
+    for(int i = 0; i < 8; i++)
+    {
+       if(a[x+h[i]][y+c[i]] == '*') res++;
+    }
+    return res;
+}
 int main(){
     int col , row;
     cin >> row >> col;
-    char a[row][col];
-    for (int i = 0; i < row; i ++){
-        for (int j = 0; j < col; j++){
+    for (int i = 1; i < row + 1; i ++){
+        for (int j = 1; j < col + 1; j++){
             cin >> a[i][j];
-            if (a[i][j] == '.') a[i][j] = '0';
-        }
-    }
-     for (int i = 0; i < row; i ++){
-        for (int j = 0; j < col; j++){
-
-            if (i == 0){
-                if (j == 0)
-            }
         }
     }
 
-
-
-     for (int i = 0; i < row; i ++){
-        for (int j = 0; j < col; j++){
-           cout << a[i][j] << " ";
+     for(int i = 1; i < row + 1; i++)
+    {
+        for(int j = 1; j < col + 1; j++)
+        {
+            if(a[i][j]=='*') cout << a[i][j] << " ";
+            else cout << dmin(i,j)<<" ";
         }
         cout << endl;
     }
-
 }
